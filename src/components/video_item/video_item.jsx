8a3d00ab580d688def25374482ed7cae0e551de5
sangsang6914 <sangsang6914@bandisnc.com>
2022-09-29 16:07:53 +0900

    video list component 및 css 추가

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import styles from './video_item.module.css'
 
-function VideoItem(props) {
+function VideoItem({video: {snippet}}) {
   return (
-    <h1>{props.video.snippet.title}</h1>
+    <>
+        <li className={styles.container}>
+            <div className={styles.video}>
+                <img className={styles.thumbnail} src={snippet.thumbnails.medium.url} alt="video thumbnail" />
+                <div className={styles.metadata}>
+                    <p className={styles.title}>{snippet.title}</p>
+                    <p className={styles.channelTitle}>{snippet.channelTitle}</p>
+                </div>
+            </div>
+        </li>
+    </>
   )
 }
 
